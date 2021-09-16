1a0303dbf31b3de236ad4196513443917483876b
sanika2106 <89378054+sanika2106@users.noreply.github.com>
2021-09-16 19:20:53 +0530

    Add files via upload

---
+++ guessing game.py
@@ -0,0 +1,23 @@
+# Now, we will make a game using loops. We will call this game a guessing game.
+# In this game we take any number, let us suppose this number is number 5.
+# After this we take any number as an input from the user between 1 to 10. The user tries to guess 
+# this number.
+# Suppose the user gives 3as an input. We will then check if 3 is equal to 5 or not?
+# 3 is not equal to 5 so we will ask the user for another input.
+# Now, we will check if that number is equal to 5 or not.
+# User will get 5 chances to guess.
+# If he guessed right within the 5 chances he wins and if he guesses wrong then loses the game.
+# Hint :
+# Study about break statement`` in python.
+
+i=1
+while i<=10:
+    num=int(input("enter the number:"))
+    if num==5:
+        print("you win")
+        break
+i=i+1
+
+
+
+
