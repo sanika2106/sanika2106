40a569468468873c221a7b400f6c426b9bf4e305
sanika2106 <89378054+sanika2106@users.noreply.github.com>
2021-09-17 10:14:05 +0530

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,6 @@
+fruit=["apple","banana","cherry"]
+for x in fruit:
+    if x == "banana":
+      continue
+    print(x)
+    
---
+++ 23.py
@@ -0,0 +1,14 @@
+fruit=["apple","banana","cherry"]
+for x in fruit:
+   print(x)
+   if x=="banana":
+     break
+
+
+fruit=["apple","banana","cherry"]
+for x in fruit:
+    if x=="banana":
+     break
+    print(x)
+   
+   
