93c5d75d49cb262a0388af22f00861f6ce75814d
sanika2106 <89378054+sanika2106@users.noreply.github.com>
2021-10-19 09:49:47 +0530

    Add files via upload

--- prime_number.py
+++ prime_number.py
@@ -1,12 +1,14 @@
 
 # prime number
 
-i=1
-while i<=100:
-    if i%2!=0 and i%3!=0 and i%5!=0:
+num=int(input("enter the number:"))
+i=num 
+while i<=num:
+    if i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0:
         print("prime number",i)
-    elif i==2 or i==3 or i==5:
+    elif i==2 or i==3 or i==5 or i==7:
         print("prime number",i)
     else:
         print("not prime number",i)
     i+=1 
+
